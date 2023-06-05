b74c56f997477fb562c2e50b8cd82d1dc4d35c47
MERYEM ÇİFCİ <MERYEM FC@DESKTOP-HEEOSFC>
2023-06-05 16:37:40 +0300

    first commit

---
+++ SecondRead-master/Anasayfa/script.js
@@ -0,0 +1,8 @@
+function submitForm() {
+    var searchInput = document.getElementById("search-input");
+    var searchTerm = searchInput.value;
+    // Arama işlemini burada yapabilirsiniz
+    console.log("Aranan kelime: " + searchTerm);
+    return false;
+}
+
